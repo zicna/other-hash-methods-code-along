4fce944f782c1220dbb75367d79c505d9027611a
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2020-12-13 22:17:10 -0600

    commit tree 34813427b46341e563a0364fde5be74b0bce03f1

--- lib/min.rb
+++ lib/min.rb
@@ -10,4 +10,25 @@ require 'pry'
 
 def get_the_min(groceries)
   #code your solution here!
+
+  #*********my solution:
+  # array_of_min = []
+  # groceries.each do |key, value|
+  #   #puts value
+  #   array_of_min << value.min
+  # end
+  # array_of_min.min
+
+  #***** code along
+  #puts groceries.values
+  groceries.values.flatten.min
 end
+
+
+# groceries = {
+#  dairy: ["milk", "yogurt", "cheese"],
+#  vegetable: ["carrots", "broccoli", "cucumbers"],
+#  meat: ["chicken", "steak", "salmon"],
+#  grains: ["rice", "pasta"]
+# }
+# get_the_min(groceries)
